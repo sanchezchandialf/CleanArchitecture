01c1002b7a1ac612e192611f75eca51073d58dac
sanchezchandialf <lautarosanche@gmail.com>
2025-05-07 00:29:31 -0300

    segundo push

--- FuncionalParadigma/Program.cs
+++ FuncionalParadigma/Program.cs
@@ -1,4 +1,5 @@
-﻿using FuncionalParadigma;
+﻿using System.Reflection.Metadata.Ecma335;
+using FuncionalParadigma;
 var beer = new Beer()
 {
     Name = "Cerveza"
@@ -13,13 +14,13 @@ Beer ToUpper(Beer beer)
 }
 
 Action<String> show = Console.WriteLine;
-Action <int,int,int> suma=(a,b,c)=> show((a + b + c).ToString());
+Action<int, int, int> suma = (a, b, c) => show((a + b + c).ToString());
 suma(1, 2, 3);
 show("buenas tardes");
-Func<int, int, int> suma2 = (a,b) => a*b;
+Func<int, int, int> suma2 = (a, b) => a * b;
 Func<int, int, string> suma3 = (a, b) =>
 {
-    var res= (a + b).ToString();
+    var res = (a + b).ToString();
     return res;
 };
 //show(suma3(1, 2));
@@ -33,14 +34,21 @@ Beer ToUpperPure(Beer beer)
     };
     return beer2;
 }
+
+Predicate<int> espar = number => number % 2 == 0;
 List<int> number = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-var number2 = filter(number,number=>number %2 ==0);
+var number2 = filter(number, espar);
+var anita = filter(number, number => number is 1 or 4);
 foreach (var item in number2)
 {
     Console.WriteLine(item);
 }
+foreach (var item in anita)
+{
+    Console.WriteLine(item);
+}
 //predicate si o si retorna bool
-List<int>filter(List<int> list, Predicate<int> predicate)
+List<int> filter(List<int> list, Predicate<int> predicate)
 {
     var result = new List<int>();
     foreach (var item in list)
@@ -53,8 +61,17 @@ List<int>filter(List<int> list, Predicate<int> predicate)
     return result;
 }
 
-Console.WriteLine(ToUpperPure(beer).Name);
-Console.WriteLine(beer.Name);
+show(ToUpperPure(beer).Name);
+show(beer.Name);
+show(ToUpperPure(beer).Name);
+
+return NewMethod();
 
 
-Console.WriteLine(ToUpperPure(beer).Name);
+
+  static  int sumas(int a, int b)
+{
+    return a + b;
+
+}
+    
