3e965be388fd9188dd4ddad1bcc3bb889cac8629
sanchezchandialf <lautarosanche@gmail.com>
2025-05-19 11:15:00 -0300

    ToDo hecho

--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TodoCore;
+
+
+
+
+var nombre = "";
+Console.WriteLine("Ingrese el nombre de la tarea:");
+nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la descripcion de la tarea:");
+var descripcion = Console.ReadLine();
+var task = new TaskLong();
+task.AddTask(nombre, descripcion);
+var list = new TaskList<TaskLong>();
+list.AddTask(task);
+
+
--- TodoCore/Class1.cs
+++ TodoCore/Class1.cs
@@ -1,58 +1,117 @@
-﻿namespace TodoCore
+﻿using System.Xml.Linq;
+
+namespace TodoCore
 {
     public class Task
     {
-        public int number = 0;
-        public int Id { get; set; }
+        private static int counter = 0;
+        public int Id { get; private set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public Task()
-        {
-        }
+        public Task() { }
 
         public Task(string name, string description)
         {
-            Id = number + 1;
+            Id = ++counter;
             Name = name;
             Description = description;
-            number++;
         }
 
+        public override string ToString()
+        {
+            return $"{Id}. {Name} - {Description}";
+        }
     }
 
-    public interface ITaskList
+    public interface ITaskList<T>
     {
-        public void AddTask();
-        public void RemoveTask();
-        public void UpdateTask();
-
+        public void AddTask(string name,string descripction);
+        public void Get();
     }
 
-
-    public class TaskManager : ITaskList
+    public class TaskShort : Task,ITaskList<Task>
     {
-        protected List<Task> _tasks;
-
-        public void AddTask()
-        {
-            Console.WriteLine("Enter the name of the task:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter the description of the task:");
-            string description = Console.ReadLine();
-            Task task = new Task(name, description);
-            _tasks.Add(task);
-        }
-
-        public void RemoveTask()
+        public TaskShort() { } 
+        public TaskShort(string name, string description) : base(name, description)
         {
         }
-        public void UpdateTask()
+        public void AddTask(string name, string descripcion)
         {
+            if (descripcion.Length < 200)
+            {
+                var task = new TaskShort(name, descripcion);
+                Console.WriteLine($"Tarea agregada: {task.Name} - {task.Description}");
+            }
+            else
+            {
+                Console.WriteLine("La descripcion es muy larga");
+
+            }
         }
-
-
-
+        public void Get()
+        {
+            Console.WriteLine("Get Task");
+        }
     }
+
+    public class TaskLong : Task, ITaskList<Task>
+    {
+
+        public TaskLong() { }
+        public TaskLong(string name, string description) : base(name, description)
+        {
+        }
+        public void AddTask(string name, string descripcion)
+        {
+            if (descripcion.Length >= 200)
+            {
+                var task = new TaskLong(name, descripcion);
+                Console.WriteLine($"Tarea agregada: {task.Name} - {task.Description}");
+            }
+            else
+            {
+                Console.WriteLine("La descripcion es muy corta");
+
+            }
+        }
+        public void Get()
+        {
+            Console.WriteLine("Get Task");
+        }
+    }
+
+
+    public interface GetListTask<T>
+    {
+        public void GetTask();
+    }
+
+
+    public interface AddListTask<T>
+    {
+        public void AddTask(T generic);
+    }
+
+
+
+
+    public class TaskList<T> : GetListTask<T>, AddListTask<T>
+    {
+        protected List<T> tasks = new List<T>();
+        public void AddTask(T generic)
+        {
+          
+            tasks.Add(generic);
+            Console.WriteLine($"Tarea agregada: {generic}");
+        }
+        public void GetTask()
+        {
+            Console.WriteLine("Get Task");
+        }
+    }
+
+
+
 }
 
