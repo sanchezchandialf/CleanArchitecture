837f350f99cbc4f9ebad1af1e788d6b9d7e49c5c
sanchezchandialf <lautarosanche@gmail.com>
2025-05-25 21:51:38 -0300

    nuevas capas

---
+++ CA-InterfaceAdapters_Repository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using CA_ApplicationLayer;
+using CA_EnterpriseLayer;
+using CA_InterfaceAdapters_Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA_InterfaceAdapters_Repository
+{
+    public class Repository :Irepository 
+    {
+        private readonly AppDbContext _appDbContext;
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(Beer beer)
+        {
+            await _appDbContext.AddAsync(beer);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Beer>> GetAllAsync()
+        {
+            return await _appDbContext.Beers.ToListAsync();
+        }
+
+        public async Task<Beer> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Beers.FindAsync(id);
+        }
+
+        Task<Beer> Irepository.AddAsync(Beer beer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CA_InterfaceAdapters_Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CA_EnterpriseLayer;
+namespace CA_InterfaceAdapters_Data
+{
+    public class AppDbContext :  DbContext  
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options)
+            : base(options)
+        { }
+        public  DbSet<Beer>Beers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beer>().ToTable("Beer");
+        }
+    }
+}
